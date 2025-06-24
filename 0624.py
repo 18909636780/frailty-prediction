ce559a041d61b803f1bf313786a5a521d851a951
18909636780 <18909636780@163.com>
2025-06-24 15:38:59 +0800

    Update 0624.py

--- 0624.py
+++ 0624.py
@@ -72,8 +72,8 @@ Neutrophil_to_Lymphocyte_Ratio = st.number_input(
 Total_Cholesterol = st.number_input(
     "Total Cholesterol (mmol/L):", 
     min_value=0.0,        # 允许最小值 0.0（浮点数）
-    max_value=300.0,      # 允许最大值 300.0（浮点数）
-    value=150.0,          # 默认值改为 150.0（浮点数）
+    max_value=20.0,      # 允许最大值 300.0（浮点数）
+    value=5.0,          # 默认值改为 150.0（浮点数）
     step=0.1,             # 允许以 0.1 为步长调整（如 5.2、6.7）
     format="%.2f"         # 格式化显示 1 位小数（可选）
 )
